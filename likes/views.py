899d1a28a3cc505853d81450779f6247b1102469
Penny Train <penny.train04@gmail.com>
2024-04-06 15:45:22 +0100

    Added likes views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from my_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializer import LikeSerializer
 
 # Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_Classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
