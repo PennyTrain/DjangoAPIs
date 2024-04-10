9daf31b873422e47e9f156c92a50facd5c8d1b10
Penny Train <penny.train04@gmail.com>
2024-04-10 17:05:44 +0100

    Added JSON Web Tokens to the project and configured them

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from my_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializer import CommentSerializer, CommentDetailSerializer
@@ -11,6 +12,12 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_field = [
+        'post'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
